45f07421efa6b08012bae7637027517bc5af2f8d
misongko <misong.ko23@gmail.com>
2022-09-01 17:28:35 +0900

    정기구독 관련 파일 생성

---
+++ src/main/java/com/kh/monong/subscribe/controller/SubscribeController.java
@@ -0,0 +1,19 @@
+package com.kh.monong.subscribe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+@RequestMapping("/subscribe")
+public class SubscribeController {
+
+	// 선아코드 시작
+	// 선아코드 끝
+		
+	// 미송코드 시작
+	// 미송코드 끝
+		
+}
