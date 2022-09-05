9affb2711bbac30905ae59ae6b61a240f6315e89
Min-ky <titma326@daum.net>
2022-09-05 16:14:57 +0900

    직거래 [상품 상세페이지] .do로 수정

--- src/main/java/com/kh/monong/direct/controller/DirectController.java
+++ src/main/java/com/kh/monong/direct/controller/DirectController.java
@@ -99,7 +99,7 @@ public class DirectController {
 	
 	//----------------- 재경 끝
 	//----------------- 민지 시작
-	@GetMapping("/directProductDetail")
+	@GetMapping("/directProductDetail.do")
 	public String directProductDetail() {
 		return "direct/directProductDetail";
 	}
