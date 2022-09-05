8a1b2e48ed7d2220bd9c588d0584b869a2f87397
Min-ky <titma326@daum.net>
2022-09-05 19:27:12 +0900

    직거래 [dto] Entity 추가!

---
+++ src/main/java/com/kh/monong/direct/model/dto/DirectProductEntity.java
@@ -0,0 +1,28 @@
+package com.kh.monong.direct.model.dto;
+
+import java.time.LocalDateTime;
+
+import org.springframework.lang.NonNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DirectProductEntity {
+	@NonNull
+	private String dProductNo;
+	@NonNull
+	private String memberId;
+	@NonNull
+	private String dProductName;
+	@NonNull
+	private String dProductContent;
+	private LocalDateTime dProductCreatedAt;
+	private LocalDateTime dProductUpdatedAt;
+	private int dDefaultPrice;
+}
