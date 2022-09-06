d8606fd1446ec591c6d6c9315a5fd59255e75e7b
misongko <misong.ko23@gmail.com>
2022-09-06 19:37:47 +0900

    subscribe - 구독플랜 관련 파일 및 코드 추가
    sPlan.css, subscribePlan.jsp 파일 추가
    Subscribe 관련 Controller, Service, ServiceImpl, Dao 에 코드 추가

--- src/main/java/com/kh/monong/subscribe/model/service/SubscribeServiceImpl.java
+++ src/main/java/com/kh/monong/subscribe/model/service/SubscribeServiceImpl.java
@@ -1,12 +1,32 @@
 package com.kh.monong.subscribe.model.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.kh.monong.subscribe.model.dao.SubscribeDao;
+import com.kh.monong.subscribe.model.dto.SubscriptionProduct;
+import com.kh.monong.subscribe.model.dto.Vegetables;
 
 @Service
 public class SubscribeServiceImpl implements SubscribeService {
+	
+	@Autowired
+	private SubscribeDao subscribeDao;
+	
 	// 선아코드 시작
 	// 선아코드 끝
 		
 	// 미송코드 시작
+	@Override
+	public List<SubscriptionProduct> getSubscriptionProduct() {
+		return subscribeDao.getSubscriptionProduct();
+	}
+	
+	@Override
+	public List<Vegetables> getVegetables() {
+		return subscribeDao.getVegetables();
+	}
 	// 미송코드 끝
 }
