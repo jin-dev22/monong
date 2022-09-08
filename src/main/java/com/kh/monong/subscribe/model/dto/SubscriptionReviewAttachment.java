2db6837145652270108e525e5603c5d41b25cd5b
misongko <misong.ko23@gmail.com>
2022-09-08 18:19:54 +0900

    subscribe - SubscriptionReviewAttachment.java에 어노테이션 추가 및 필드명 변경

--- src/main/java/com/kh/monong/subscribe/model/dto/SubscriptionReviewAttachment.java
+++ src/main/java/com/kh/monong/subscribe/model/dto/SubscriptionReviewAttachment.java
@@ -2,10 +2,19 @@ package com.kh.monong.subscribe.model.dto;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SubscriptionReviewAttachment {
 	private int s_attach_no;
 	private String s_review_no;
-	private String sReviewOriginFilename;
+	private String sReviewOriginalFilename;
 	private String sReviewRenamedFilename;
 	private LocalDateTime sReviewCreatedAt;
 }
