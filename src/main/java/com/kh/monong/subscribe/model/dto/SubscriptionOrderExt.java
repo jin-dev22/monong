a1d71dc8e722afb1b6d26a626562e11157fd4af1
misongko <misong.ko23@gmail.com>
2022-09-21 18:53:36 +0900

    SubscriptionOrderExt.java - SubscriptionOrder.java가 변경되면서 생긴 오류 해결

--- src/main/java/com/kh/monong/subscribe/model/dto/SubscriptionOrderExt.java
+++ src/main/java/com/kh/monong/subscribe/model/dto/SubscriptionOrderExt.java
@@ -20,15 +20,14 @@ import lombok.ToString;
 public class SubscriptionOrderExt extends SubscriptionOrder {
 	private String sReviewNo;
 
-	public SubscriptionOrderExt(String sOrderNo, @NonNull String sNo, int sTimes, int sPrice, LocalDateTime sOrderDate,
+	public SubscriptionOrderExt(String sOrderNo, @NonNull String sNo, int sTimes, int sPrice, LocalDate sOrderDate,
 			String sOrderStatus, int soCardInfoNo, String soProductCode, String soExcludeVegs, int soDeliveryCycle,
 			LocalDate soDeliveryDate, YN soDelayYn, String soRecipient, String soPhone, String soAddress,
-			String soAddressEx, String soDeliveryRequest, String sReviewNo) {
+			String soAddressEx, String soDeliveryRequest, LocalDate soDeliveryCompletedDate, String sReviewNo) {
 		super(sOrderNo, sNo, sTimes, sPrice, sOrderDate, sOrderStatus, soCardInfoNo, soProductCode, soExcludeVegs,
 				soDeliveryCycle, soDeliveryDate, soDelayYn, soRecipient, soPhone, soAddress, soAddressEx,
-				soDeliveryRequest);
+				soDeliveryRequest, soDeliveryCompletedDate);
 		this.sReviewNo = sReviewNo;
 	}
-	
-	
+
 }
